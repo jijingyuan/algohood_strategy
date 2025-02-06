f98ff03821082301a434d69d93a1ac5c8ed04d68
jijingyuan <jijingyuan@rookiequant.com>
2025-02-06 11:06:26 +0800

    Update init_redis.py to download DOGE/USDT trade data
    - Change symbols from list to single string 'doge_usdt'
    - Maintain existing download and sync functionality
    - Prepare for specific cryptocurrency trade data retrieval

--- algoStart/init_redis.py
+++ algoStart/init_redis.py
@@ -14,11 +14,10 @@ config_port = 6001  # config port
 node_port = 9001  # node port
 
 # download data from binance data server
-symbols = ['eth_usdt']  # this could be a list of symbols like ['btc_usdt', 'eth_usdt']
+symbols = 'doge_usdt'  # this could be a list of symbols like ['btc_usdt', 'eth_usdt']
 start_dt = '2025-01-01'
 end_dt = '2025-01-21'
 BrokerMgr.download_trades(symbols, start_dt, end_dt)
-
 
 # sync data from folder to redis
 BrokerMgr.sync_redis(redis_host, config_port, node_port)
